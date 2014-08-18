9e221ca2ba128f66486f4851555dee04cc8701db
erik <erik@pop-ad.com>
2014-08-17 19:33:22 -0700

    New DB
    ‘New’ DB for new tags that haven’t been shown yet.

--- server/tagCall.js
+++ server/tagCall.js
@@ -3,8 +3,6 @@
 instagram = new Instagram.createClient('5166661f18554a699feaed3de378c3bf', 'bfebf48354df4928a0aab8efec13d906');
 Fiber = Npm.require('fibers');
 
-var next = '0';
-
 // Tag Count
 instagram.tags.tag('pamurico', function (tag, err) { 
 	try {
@@ -17,14 +15,23 @@ instagram.tags.tag('pamurico', function (tag, err) {
 	}
 });
 
-instagram.tags.media('pamurico', {max_tag_id: next}, function (tag, err, pag) { // for max_tag_id get next_max_tag_id from pag
-	console.log(pag);
-	try {
-		Fiber(function() { 
-			for (i = 0; i < 20; ++i) Tags.insert(tag[i]);
-		}).run();
-	} catch (err) {
-		throw err;
-	}
-});
+var instaDB = function (pagi) {
+	instagram.tags.media('pamurico', {max_tag_id: pagi}, function (tag, err, pag) { // for max_tag_id get next_max_tag_id from pag
+		try {
+			Fiber(function() { 
+				// Tag to DB
+				for (i = 0; i < tag.length; ++i) { 
+					Tags.insert(tag[i]);
+					New.insert(tag[i]);
+				}; 
 
+				Pagi.insert(pag); // Pagination to DB
+			}).run();
+		} catch (err) {
+			throw err;
+		}
+	});
+}
+
+instaDB('0');
+instaDB('1402721579746994');
