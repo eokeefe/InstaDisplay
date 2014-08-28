a5c7e675c3a1c8449745ba33701134cecbccd6ee
seano314 <sokeefe@googl.com>
2014-08-27 22:00:39 -0700

    Adding helper files

--- client/views/currentTag.helper.js
+++ client/views/currentTag.helper.js
@@ -1,3 +1,16 @@
+// FOR Production once finished!
+// Template.currentTag.helpers({
+// 	Tag: function () {
+// 		// var T = Tags.find();
+// 		// var url = T.images.stand.... // for loop
+// 		return Tags.find();
+// 	}
+// });
+
+
+
+
+// FOR TESTING BACKEND ONLY!
 Template.currentTag.helpers({
 	Tag: function (i) {
 		var photos = Tags.find().fetch();
