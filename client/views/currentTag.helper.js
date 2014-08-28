7348a8651cc1cfb918ed42f4131300c9acbfc8e6
Sean O'Keefe <seano314@users.noreply.github.com>
2014-08-28 08:45:13 -0700

    Adding Photo helper

--- client/views/currentTag.helper.js
+++ client/views/currentTag.helper.js
@@ -1,20 +1,6 @@
-// FOR Production once finished!
-// Template.currentTag.helpers({
-// 	Tag: function () {
-// 		// var T = Tags.find();
-// 		// var url = T.images.stand.... // for loop
-// 		return Tags.find();
-// 	}
-// });
-
-
-
-
-// FOR TESTING BACKEND ONLY!
+// For Production once finished!
 Template.currentTag.helpers({
-	Tag: function (i) {
-		var photos = Tags.find().fetch();
-
-		return photos[i].images.standard_resolution.url;
+	Tag: function () {
+		return Tags.find();
 	}
 });
